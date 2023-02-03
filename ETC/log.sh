0c20abc363741808d0e3657541f5bc8e73bd2f6b
Adrian Ardizza <adrian.ardizza@gmail.com>
2023-02-04 04:46:48 +0700

    Fix minor issue with ZCZC log script

--- ETC/log.sh
+++ ETC/log.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-LOG_TYPE=$(curl https://osp4diss.vlsm.org/ETC/logCodes.txt | awk '$1 ~ /^L/' | fzf)
-LOG_CODE=$(echo $LOG_TYPE | cut -d " " -f1)
 LOG_WEEKS=(W00 W01 W02 W03 W04 W05 W06 W07 W08 W09 W10 W11)
 
 function serialize_array() {
@@ -11,8 +9,17 @@ function serialize_array() {
         _str="${_array[*]}" # * => join on IFS
 }
 
+LOG_TYPE=$(curl -s https://osp4diss.vlsm.org/ETC/logCodes.txt | awk '$1 ~ /^L/' | fzf)
+LOG_CODE=$(echo $LOG_TYPE | cut -d " " -f1)
+if [ "$LOG_CODE" == "" ]; then
+    exit 0
+fi
+
 serialize_array LOG_WEEKS LOG_WEEK_STRING $'\n'
 LOG_WEEK=$(echo "$LOG_WEEK_STRING" | fzf)
+if [ "$LOG_WEEK" == "" ]; then
+    exit 0
+fi
 
 echo
 read -p "Enter Time Allocated: " LOG_TIME
